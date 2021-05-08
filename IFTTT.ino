a3b7f9b772479921082988a6229adbeab723cf88
Ben Marriner <goolog2@hotmail.com>
2021-05-08 12:33:03 +1000

    Add files via upload

---
+++ IFTTT.ino
@@ -0,0 +1,20 @@
+#include <BH1750Lib.h>
+
+BH1750Lib sensor;
+int lux;
+
+void setup()
+{
+    Particle.variable("Luminosity", &lux, INT);
+    sensor.begin(BH1750LIB_MODE_CONTINUOUSHIGHRES2);
+    Serial.begin(9600);
+}
+
+void loop()
+{
+    Serial.print(lux);
+    Serial.print(" lx");
+    Serial.println();
+
+    delay(1000);
+}
